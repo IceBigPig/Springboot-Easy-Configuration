62eb98429f89e24869005c8ff27c5cc0488a5bf3
icebigpig <icebigpig404@foxmail.com>
2023-01-05 23:44:12 +0800

    Change springboot version 3.0.1 to 2.7.7

--- src/main/java/com/icebigpig/config/SaTokenConfigure.java
+++ src/main/java/com/icebigpig/config/SaTokenConfigure.java
@@ -2,10 +2,10 @@ package com.icebigpig.config;
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaServletFilter;
-import cn.dev33.satoken.interceptor.SaAnnotationInterceptor;
-import cn.dev33.satoken.interceptor.SaRouteInterceptor;
+import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.router.SaHttpMethod;
 import cn.dev33.satoken.router.SaRouter;
+import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,32 +20,42 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
 
+    /**
+     * 注册 Sa-Token 拦截器打开注解鉴权功能
+     */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 的路由拦截器，自定义认证规则
-        registry.addInterceptor(new SaRouteInterceptor((req, res, handler)->{
-            // 根据路由划分模块，不同模块不同鉴权
-            SaRouter.match(SaHttpMethod.OPTIONS).match("/**").check(r -> System.out.println("Option请求，放行"));
-//            SaRouter.match("/**").check(r -> StpUtil.checkLogin());
-        })).addPathPatterns("/**")
-                .excludePathPatterns("/login/**")
-                .excludePathPatterns("/init/**")
-                .excludePathPatterns("/verify/**")
-                .excludePathPatterns("/doc.html/**")
-                .excludePathPatterns("/webjars/**")
-                .excludePathPatterns("/swagger-resources")
-                .excludePathPatterns("/v2/api-docs")
-                .excludePathPatterns("/distribution/get-score-by-rank/**");
+        // 注册 Sa-Token 拦截器打开注解鉴权功能
+        registry.addInterceptor(new SaInterceptor(handle -> {
+            // SaManager.getLog().debug("----- 请求path={}  提交token={}", SaHolder.getRequest().getRequestPath(), StpUtil.getTokenValue());
 
-        registry.addInterceptor(new SaAnnotationInterceptor()).addPathPatterns("/**")
-                .excludePathPatterns("/user/**")
-                .excludePathPatterns("/init/**")
-                .excludePathPatterns("/verify/**")
-                .excludePathPatterns("/doc.html/**")
-                .excludePathPatterns("/webjars/**")
-                .excludePathPatterns("/swagger-resources")
-                .excludePathPatterns("/v2/api-docs")
-                .excludePathPatterns("/distribution/get-score-by-rank/**");
+            // 指定一条 match 规则
+            SaRouter
+                    .match("/user/**")    // 拦截的 path 列表，可以写多个
+                    .notMatch("/login/username", "/user/doLogin2")     // 排除掉的 path 列表，可以写多个
+                    .check(r -> StpUtil.checkLogin());        // 要执行的校验动作，可以写完整的 lambda 表达式
+
+            // 权限校验 -- 不同模块认证不同权限
+            SaRouter.match("/admin/**", r -> StpUtil.checkPermission("admin"));
+            SaRouter.match("/goods/**", r -> StpUtil.checkPermission("goods"));
+            SaRouter.match("/orders/**", r -> StpUtil.checkPermission("orders"));
+            SaRouter.match("/notice/**", r -> StpUtil.checkPermission("notice"));
+            SaRouter.match("/comment/**", r -> StpUtil.checkPermission("comment"));
+
+            // 甚至你可以随意的写一个打印语句
+            SaRouter.match("/router/print", r -> System.out.println("----啦啦啦----"));
+
+            // 写一个完整的 lambda
+            SaRouter.match("/router/print2", r -> {
+                System.out.println("----啦啦啦2----");
+                // ... 其它代码
+            });
+
+            /*
+             * 相关路由都定义在 com.pj.cases.use.RouterCheckController 中
+             */
+
+        })).addPathPatterns("/**");
     }
 
     /**
@@ -65,7 +75,7 @@ public class SaTokenConfigure implements WebMvcConfigurer {
                 .setBeforeAuth(obj -> {
                     // ---------- 设置跨域响应头 ----------
                     SaHolder.getResponse()
-                            .setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+                            .setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
                             .setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
                             .setHeader("Access-Control-Max-Age", "3600")
                             .setHeader("Access-Control-Allow-Headers", "token,Content-Type")
--- src/main/java/com/icebigpig/controller/LoginController.java
+++ src/main/java/com/icebigpig/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.icebigpig.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.icebigpig.entity.vo.Result;
 import com.icebigpig.enums.StatusCodeEnum;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 
 @Slf4j
@@ -16,9 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class LoginController {
 
+    @GetMapping("/test")
+    public Result<StatusCodeEnum> testFunction() {
+        return Result.ok();
+    }
+
     @PostMapping("/username")
     @ApiOperation(value = "通过账号登录")
-    public Result<StatusCodeEnum> doLoginByUsername() {
+    public Result<StatusCodeEnum> doLoginByUsername(String username, String password) {
+        if (Objects.equals(username, "123") && Objects.equals(password, "123")) {
+            StpUtil.login(1);
+        } else {
+            Result.fail();
+        }
         return Result.ok();
     }
 
