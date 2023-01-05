1d5825a8da4b8c2ba6cf6adc8fed7fc8e460459c
icebigpig <icebigpig404@foxmail.com>
2023-01-05 17:09:18 +0800

    Fix README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Springboot Easy Configuration
+
+------
+
+### 一个用于快速开发web后端服务的springboot配置
+
+**加入配置项，简化一些不必要的重复配置**
+
+- Sa-Token:一个简单易上手的安全认证框架
+- 全局异常捕捉配置
+- Swagger2/knife4j配置
+- Validation注解配置
+- Mybatis-Plus配置
+- 数据库连接池配置
+
--- src/main/java/com/icebigpig/controller/LoginController.java
+++ src/main/java/com/icebigpig/controller/LoginController.java
@@ -5,7 +5,9 @@ import com.icebigpig.enums.StatusCodeEnum;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @Slf4j
